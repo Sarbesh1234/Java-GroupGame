f59a948f0135c3c37f42c3245e18a7ebba8418c9
nsadik333 <nsadik333@student.fuhsd.org>
2020-05-18 15:28:46 -0700

    Changed a few variables to modify class

--- Y2020_P1_Team_5_BubbleShooter/Player.java
+++ Y2020_P1_Team_5_BubbleShooter/Player.java
@@ -1,18 +1,18 @@
 import greenfoot.*;
 public class Player extends Actor
 {
-    World w = getWorld();
     GreenfootImage img = getImage();
-    int h = img.getHeight();
-    int wi = img.getWidth();
     int life = 3;
-    
+
     public void act() 
     {
+        int h = img.getHeight();
+        int wi = img.getWidth();
+        
         if(Greenfoot.isKeyDown("space")){
             shoot();
         }
-        
+
         if(Greenfoot.isKeyDown("left")){
             setLocation(getX() - 5, getY());
         }else if(Greenfoot.isKeyDown("right")){
@@ -22,35 +22,35 @@ public class Player extends Actor
         }else if(Greenfoot.isKeyDown("down")){
             setLocation(getX(), getY() + 5);
         }
-        
-        if(getX()<1) {
+
+        if(getX()<=1) {
             setLocation(getWorld().getWidth()-10,getY());
         }
-        
+
         if(getX()>getWorld().getWidth()-5) {
             setLocation(10,getY());
         }
-        
+
         if(getY()+h/2>getWorld().getHeight()) {
             setLocation(getX(),getWorld().getHeight()-h/2);
         }
-    if(getY()-h/2<(getWorld().getHeight()/5)*4) {
+        if(getY()-h/2<(getWorld().getHeight()/5)*4) {
             setLocation(getX(),(getWorld().getHeight()/5)*4+h/2);
         }
-        
+
         asteroidHit();
-        
+
         if(life == 0){
             getWorld().removeObject(this);
             Greenfoot.stop(); 
         }
     }    
-    
+
     public void shoot() {
         Laser l = new Laser();
         getWorld().addObject(l, getX(), getY() - getImage().getHeight()/2);
     }
-    
+
     public void asteroidHit(){
         if(getOneIntersectingObject(Asteroid.class) != null){
             life = life - 1;

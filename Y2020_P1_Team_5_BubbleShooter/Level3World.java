b1ac24e376a07257131e5587c433528e22a05ba9
nsadik333 <nsadik333@student.fuhsd.org>
2020-05-29 02:40:44 -0700

    Level 3 World Spawning

--- Y2020_P1_Team_5_BubbleShooter/Level3World.java
+++ Y2020_P1_Team_5_BubbleShooter/Level3World.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Level3World extends World
 {
-
+    Counter counter = new Counter();
+    Player player = new Player();
+    Alien alien = new Alien();
     /**
      * Constructor for objects of class Level3World.
      * 
@@ -17,5 +19,24 @@ public class Level3World extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+    }
+
+    public void addAsteroid(){
+        if(Greenfoot.getRandomNumber(100) <= 20){
+            addObject(new Level2Asteroid(), Greenfoot.getRandomNumber(getWidth()), 0);
+        }
+    }
+
+    public void addAlien(){
+        if(Greenfoot.getRandomNumber(100) <= 2){
+            alien.setRotation(180);
+            addObject(new Alien(), Greenfoot.getRandomNumber(getWidth()), 0);
+        }
+    }
+
+    public void act() {
+        addAsteroid();
+        addAlien();
+
     }
 }
